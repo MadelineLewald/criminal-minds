a0e1196b227d086aff76d745cffa87bac010cb7d
Madeline Lewald <madelinelewald@Madelines-MacBook-Pro.local>
2020-03-23 17:39:20 -0400

    added create and delete routes

---
+++ public/js/app.js
@@ -0,0 +1,48 @@
+const app = angular.module('MyApp', []);
+
+app.controller('MyController', ['$http', function($http){
+  this.name = null;
+  this.conviction = null;
+  this.sentence = null;
+  this.image = null;
+
+  const controller = this;
+
+  this.createJailbird = function(){
+    $http({
+      method: 'POST',
+      url: '/jailbirds',
+      data: {
+        name: this.name,
+        conviction: this.conviction,
+        sentence: this.sentence,
+        image: this.image
+      }
+    }).then(
+      function(response){
+        controller.name = null;
+        controller.conviction = null;
+        controller.sentence = null;
+        controller.image = null;
+        controller.getJailbirds();
+      },
+      function(error){
+        console.log(error);
+      }
+    )
+  }
+
+  this.deleteJailbird = function(jailbird){
+    $http({
+      method: 'DELETE',
+      url: '/jailbirds/' + jailbird._id
+    }).then(
+      function(){
+        controller.getJailbirds();
+      },
+      function(error){
+        
+      }
+    )
+  }
+}]);
